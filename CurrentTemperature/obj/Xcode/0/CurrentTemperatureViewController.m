13cabe5500336692a767f82807da39231f5eb388
Mark Fricke <mfricke@Mark-Macbook.local>
2012-07-26 13:38:29 -0500

    Initial example checkin

---
+++ CurrentTemperature/obj/Xcode/0/CurrentTemperatureViewController.m
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by MonoDevelop to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "CurrentTemperatureViewController.h"
+
+@implementation CurrentTemperatureViewController
+
+@synthesize lblTemp = _lblTemp;
+@synthesize btnRefresh = _btnRefresh;
+
+- (IBAction)Refresh:(id)sender {
+}
+
+@end
